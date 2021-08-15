ed2f9d397b09f5b03776e77659ea0800e0bb5e2b
James Driscoll <thejamesdriscoll@gmail.com>
2021-08-15 12:16:18 -0500

    made grid

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let container = document.querySelector('#container')
-container.textContent = ""
 
- /*function genDivs (size) {
- grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
- grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-}
-*/
-function genDivs (size) {
-    let e = container
-    for (var i = 0; i < size; i++) {
-        let row = document.createElement('div')
-        row.className = "row"
-        e.appendChild(row)
-        for (let x = 1; x < size; x++) {
-            let cell = document.createElement('div')
-            cell.className = "grid"
-            row.appendChild(cell)
-        }
-    }
-    //e.appendChild(row)
+let size = 16;
+let gridContainer = document.querySelector('#grid-container')
+const maxWidth = 400;
+const maxHeight = 400;
+function makeBlocks() {
+   
     
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement('div');
+    row.className = "row";
+    
+    for (let j = 0; j < size; j++) {
+      let box = document.createElement('div');
+      box.className = "box";
+      row.appendChild(box);
+    }
+    document.getElementById('grid-container').appendChild(row);
+    
+    var boxes = document.getElementsByClassName("box");
+    for (k = 0; k < boxes.length; k++) {
+      boxes[k].style.width = maxWidth / size + "px";
+      boxes[k].style.height = maxWidth / size + "px";
+    }
+  }
 }
-genDivs(5)
+makeBlocks()
